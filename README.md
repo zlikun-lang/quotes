618ece4b81911ac793a3cb099842571904a7327b
zlikun <zlikun-dev@hotmail.com>
2018-07-17 16:09:14 +0800

    实现最基础的爬虫

--- README.md
+++ README.md
@@ -3,11 +3,42 @@
 - https://docs.scrapy.org/en/latest/
 - https://docs.scrapy.org/en/latest/
 
+#### 创建工程
+```
+# 使用默认模板创建工程
+$ scrapy startproject quotes
+New Scrapy project 'quotes', using template directory 'd:\\program\\python\\python37\\lib\\site-packages\\scrapy\\templates\\project', created in:
+    ...
+
+$ cd quotes
+
+# 生成爬虫，注意爬虫名称不能与工程名称相同
+$ scrapy genspider quote quotes.toscrape.com
+Created spider 'quote' using template 'basic' in module:
+  quotes.spiders.quote
+```
 
 #### 目标网站
 - http://quotes.toscrape.com/
 
 爬虫任务
 - 获取网页中的名言、作者、标签信息
+```
+# 运行爬虫
+$ scrapy crawl quote
+```
 - 将爬取的数据生成json/xml/csv等格式文件保存
+```
+# 运行爬虫，将结果保存成一个JSON文件(组织为一个数组)
+$ scrapy crawl quote -o .data/quotes.json
+# 运行爬虫，将结果保存成一个JSON文件(一条记录一行) 
+$ scrapy crawl quote -o .data/quotes.jl
+# 运行爬虫，将结果保存成一个XML文件
+$ scrapy crawl quote -o .data/quotes.xml
+# 运行爬虫，将结果保存成一个CSV文件
+$ scrapy crawl quote -o .data/quotes.csv
+```
 - 将爬取的数据写入MongoDB
+```
+
+```
--- quotes/spiders/quote.py
+++ quotes/spiders/quote.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from quotes.items import QuotesItem
+
 
 class QuoteSpider(scrapy.Spider):
     name = 'quote'
@@ -8,4 +10,22 @@ class QuoteSpider(scrapy.Spider):
     start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        # print(response.text)
+
+        # 抽取列表数据
+        for quote in response.css('div.col-md-8 > div.quote'):
+            item = QuotesItem()
+            # 抽取名言
+            item['text'] = quote.css('span.text::text').extract_first()
+            # 抽取作者
+            item['author'] = quote.css('small.author::text').extract_first()
+            # 抽取标签
+            item['tags'] = quote.css('div.tags > a::text').extract()
+
+            yield item
+
+        # 抽取下一页链接
+        next_href = response.css('li.next > a::attr(href)').extract_first()
+        if next_href:
+            next_url = response.urljoin(next_href)
+            yield scrapy.Request(url=next_url, callback=self.parse)
